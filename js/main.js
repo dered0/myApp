c4d55795e3387a740427cac3ae22961750d25a5f
Kamil M <kamil.makosa13@gmail.com>
2018-05-21 06:10:35 +0200

    Fixed texarea input

--- js/main.js
+++ js/main.js
@@ -5,9 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
   });
 
   $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
+    $('input#input_text, textarea#textarea1').characterCounter();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
   });
